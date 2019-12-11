55867d57f9016c14df80a7ec89861ef7cf51582f
Vik Akam <vikramakam@hotmail.com>
2019-12-11 12:59:44 -0800

    initialized repo

---
+++ README.md
@@ -0,0 +1,60 @@
+# Project: Summary, Data
+
+**Author**: Vik Akam
+**Version**: 2.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+- Start a Node.js server that will act as a back-end for receiving a search query for a city. Upon the query, process the query and return data to the front-end regarding the city's location and weather. 
+- 
+
+## Getting Started
+- Deploy a server through Node.js and Express. 
+- Get query and process query through a function that obtains data from .json files that contain location and weather data. 
+- Return the results through a response to the front-end. 
+- Test output by deploying the back-end and aplying it to the front end to see if weather and location data are being shown on the front-end. 
+
+## Architecture (Back-end)
+- JavaScript
+- Node.js
+- Express
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+## Change Log
+- 12/10/19 11am: location working
+- 12/11/19 11am: weather working
+- 12/11/19 12pm: errors working
+
+## Credits and Collaborations
+- Express
+
+## Time estimate
+
+- Number and name of feature: Feature 2, Location
+
+- Estimate of time needed to complete: 2 hours
+
+- Start time: 10am
+
+- Finish time: 12pm
+
+- Actual time needed to complete: 2 hours
+
+- Number and name of feature: Feature 3, Weather
+
+- Estimate of time needed to complete: 2 hours
+
+- Start time: 12pm
+
+- Finish time: 2pm
+
+- Actual time needed to complete: 4 hours
+
+- Number and name of feature: Feature 4, Errors
+
+- Estimate of time needed to complete: 1 hour
+
+- Start time: 11am
+
+- Finish time: 12pm
+
+- Actual time needed to complete: 1 hour
---
+++ server.js
@@ -0,0 +1,118 @@
+'use strict';
+
+// server build
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3001;
+
+// globals
+const geoData = require('./data/geo.json');
+const addressComponents = geoData.results[0].address_components[0];
+const errorMessage = {
+  status: 500,
+  responseText: 'Sorry, something went wrong',
+};
+
+app.get('/location', (request, response) => {
+  try {
+    let city = request.query.data;
+
+    if (city.toLowerCase() !== addressComponents.long_name.toLowerCase() || city !== addressComponents.short_name.toLowerCase()) {
+      response.status(500).send(errorMessage);
+    }
+
+    else {
+      let locationObject = searchLatLong(city);
+
+      response.send(locationObject);
+    }
+  }
+  catch (error) {
+    console.error(error);
+
+    response.status(500).send(errorMessage);
+  }
+});
+
+let searchLatLong = city => {
+  let resultsNav = geoData.results[0];
+
+  const latLongObj = new Location(city, resultsNav);
+
+  return latLongObj;
+};
+
+function Location(city, resultsNav) {
+  // eslint-disable-next-line camelcase
+  this.search_query = city;
+  // eslint-disable-next-line camelcase
+  this.formatted_query = resultsNav.formatted_address;
+  this.latitude = resultsNav.geometry.location.lat;
+  this.longitude = resultsNav.geometry.location.lng;
+}
+
+app.get('/weather', (request, response) => {
+  try {
+    let city = request.query.data;
+
+    if (city.toLowerCase() !== addressComponents.long_name.toLowerCase() || city !== addressComponents.short_name.toLowerCase()) {
+      response.status(500).send(errorMessage);
+    }
+    else {
+      let dailyForecastForCity = dailyWeather();
+      response.send(dailyForecastForCity);
+    }
+  }
+  catch (error) {
+    console.error(error);
+
+    response.status(500).send(errorMessage);
+  }
+});
+
+let dailyWeather = () => {
+  const weatherData = require('./data/darksky.json');
+
+  const dailyData = weatherData.daily.data;
+
+  let summary;
+  let time;
+  let dailyArray = [];
+  dailyData.forEach(day => {
+    const pairData = Object.entries(day);
+    pairData.forEach((pair) => {
+      pair.forEach((element) => {
+        if (element === 'time') {
+          time = pair[1];
+        }
+        if (element === 'summary') {
+          summary = pair[1];
+        }
+      });
+    });
+
+    // push to array
+    let eachDay = new Forecast(summary, time);
+    dailyArray.push(eachDay);
+  });
+
+  return dailyArray;
+};
+
+function Forecast(summary, time) {
+  this.forecast = summary;
+  this.time = time;
+}
+
+app.get('*', (request, response) => {
+  response.status(404).send('Page not found');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
